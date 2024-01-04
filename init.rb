e611a5fbe41ec7d1ef4e7fdb34fbcead5e648aae
sk-ys <yskrmt@gmail.com>
2024-01-04 21:14:21 +0900

    v0.1.1

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_note_author_filter do
   name 'Redmine Note Author Filter plugin'
   author 'sk-ys'
   description 'This is a plugin for Redmine'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/sk-ys/redmine_note_author_filter'
   author_url 'https://github.com/sk-ys'
 end
